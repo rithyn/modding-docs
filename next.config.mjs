af3cc50dfad8383b8cb41131c16ae96d17bd0a25
Rithy <rithynathan@gmail.com>
2024-11-24 14:33:40 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,17 @@
 import nextra from 'nextra'
  
-// /**
-//  * @type {import('next').NextConfig}
-//  */
-// const nextConfig = {
-//   output: 'export',
-//   images: {
-//     unoptimized: true // mandatory, otherwise won't export
-//   },
-//   distDir: 'frontend',
-//   // Optional: Change the output directory `out` -> `dist`
-//   // distDir: "build"
-// }
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true // mandatory, otherwise won't export
+  },
+  distDir: 'frontend',
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: "build"
+}
 const withNextra = nextra({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.jsx'
